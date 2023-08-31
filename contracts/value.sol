2bbf0f4c8635a0017f4032d4d881b16445afc2da
Anurag Dutta <imanurag18@gmail.com>
2023-08-31 14:30:45 +0530

    changed

--- contracts/value.sol
+++ contracts/value.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity >=0.4.22 <0.9.0;
 
-contract value{
-    string chains;
+contract value {
+  string chains;
 
     constructor(){
         chains="Anurag";
